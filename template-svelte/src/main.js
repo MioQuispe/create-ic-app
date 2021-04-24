8ac302e26ced0252a5e03f6642ae12bdbd21024e
MioQuispe <mioq91@gmail.com>
2021-04-24 23:21:36 +0200

    Fix issues

--- template-svelte/src/main.js
+++ template-svelte/src/main.js
@@ -3,7 +3,7 @@ import App from './App.svelte'
 /**
  * @dfinity/agent requires this. Can be removed once it's fixed
  */
-(window as any).global = window
+window.global = window
 
 const app = new App({
   target: document.getElementById('app')
